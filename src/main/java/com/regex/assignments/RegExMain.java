8e16e6fbec912e4165eb56f14faf0db2d7683f2b
sambit420 <sambit2024@gmail.com>
2022-11-19 00:03:03 +0530

    Added : Use Case 2 Program - As a user need to enter a valid last name

--- src/main/java/com/regex/assignments/RegExMain.java
+++ src/main/java/com/regex/assignments/RegExMain.java
@@ -3,8 +3,8 @@ package com.regex.assignments;
 import java.util.regex.Pattern;
 
 public class RegExMain {
-    public boolean validateFirstName(String firstName) {
+
+    public boolean validateName(String firstName) {
         return Pattern.matches("[A-Z][a-z]{3,}",firstName);
     }
 }
-
--- src/test/java/com/regex/test/RegExTest.java
+++ src/test/java/com/regex/test/RegExTest.java
@@ -1,9 +1,9 @@
 package com.regex.test;
-
 import com.regex.assignments.RegExMain;
 import org.junit.Assert;
-import org.junit.*;
-import java.util.Scanner;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 public class RegExTest {
 
@@ -16,23 +16,47 @@ public class RegExTest {
     public void validateFirstNameTest() {
         RegExMain obj = new RegExMain();
         System.out.println("Valid first name of user");
-        String firstName = "Sambit";
-        Assert.assertTrue(obj.validateFirstName(firstName));
+        String firstName = "Soumya";
+        Assert.assertTrue(obj.validateName(firstName));
     }
 
     @Test
     public void validateFirstNameLowerCaseShouldFail() {
         RegExMain obj = new RegExMain();
         System.out.println("Invalid first name of user which contains only lowercase");
-        String firstName = "sambit";
-        Assert.assertFalse(obj.validateFirstName(firstName));
+        String firstName = "soumya";
+        Assert.assertFalse(obj.validateName(firstName));
     }
 
     @Test
     public void validateFirstNameLessThanThree() {
         RegExMain obj = new RegExMain();
         System.out.println("Invalid first name of user which having only three characters");
-        String firstName = "Sam";
-        Assert.assertFalse(obj.validateFirstName(firstName));
+        String firstName = "Sou";
+        Assert.assertFalse(obj.validateName(firstName));
+    }
+
+    @Test
+    public void validateLastNameTest() {
+        RegExMain obj = new RegExMain();
+        System.out.println("Valid last name of user");
+        String firstName = "Ranjan";
+        Assert.assertTrue(obj.validateName(firstName));
+    }
+
+    @Test
+    public void validateLastNameLowerCaseShouldFail() {
+        RegExMain obj = new RegExMain();
+        System.out.println("Invalid last name of user which contains only lowercase");
+        String firstName = "ranjan";
+        Assert.assertFalse(obj.validateName(firstName));
+    }
+
+    @Test
+    public void validateLastNameLessThanThree() {
+        RegExMain obj = new RegExMain();
+        System.out.println("Invalid last name of user which having only three characters");
+        String firstName = "Ran";
+        Assert.assertFalse(obj.validateName(firstName));
     }
 }
