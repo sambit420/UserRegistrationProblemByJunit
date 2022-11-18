cc8694eef0a424c48d10c8f84a8d1090b9c9294a
sambit420 <sambit2024@gmail.com>
2022-11-18 23:47:32 +0530

    Added : Use Case 1 Program - As a user need to enter a valid first name

---
+++ src/main/java/com/regex/assignments/RegExMain.java
@@ -0,0 +1,10 @@
+package com.regex.assignments;
+
+import java.util.regex.Pattern;
+
+public class RegExMain {
+    public boolean validateFirstName(String firstName) {
+        return Pattern.matches("[A-Z][a-z]{3,}",firstName);
+    }
+}
+
